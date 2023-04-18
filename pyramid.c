28bbc43b19fbf680b803483f8d8f1513e06ed907
alv2y3 <alv2y3@clark-r630-login-node907.cobalt.lan>
2023-04-18 15:15:43 -0500

    Add MyAscii.c file

--- pyramid.c
+++ pyramid.c
@@ -3,6 +3,9 @@ int main() {
    int i, space, rows, k = 0;
    printf("Enter the number of rows: ");
    scanf("%d", &rows);
+   
+   printf("My name is Andrew Veach \n");
+
    for (i = 1; i <= rows; ++i, k = 0) {
       for (space = 1; space <= rows - i; ++space) {
          printf("  ");
@@ -15,3 +18,12 @@ int main() {
    }
    return 0;
 }
+
+
+
+
+
+
+
+
+
